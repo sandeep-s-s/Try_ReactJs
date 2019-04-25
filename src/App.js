35bcabc47224eb91081c3bb88c01ef9cd1a7e701
Sandeep <sandeepsreekumar75@gmail.com>
2019-04-26 00:19:15 +0530

    Display JSON data

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-import React from 'react'
-import logo from './logo.svg'
+import React, { Component } from 'react'
 import './App.css'
 import PostList from './posts/PostList'
-function App () {
-  return (
-    <div className='App'>
-      <PostList />
-    </div>
-  )
+class App extends Component {
+  render () {
+    return (
+      <div className='App'>
+        <PostList />
+      </div>
+    )
+  }
 }
 
 export default App
--- src/posts/PostList.js
+++ src/posts/PostList.js
@@ -1,9 +1,23 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-function PostList () {
-  return (
-    <p>Hello World</p>
-  )
+import PostData from '../data/posts.json'
+
+class PostList extends Component {
+  render () {
+    return (
+      <div >
+        <h1>React Js</h1>
+        <ul>
+          {PostData.map((data, i) => (
+            <div key={i}>
+              <b>{data.title} on {data.date}</b> :
+              {data.content}
+            </div>
+          ))}
+        </ul>
+      </div>
+    )
+  }
 }
 
 export default PostList
