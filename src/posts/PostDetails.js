6bb914e98a10e7dabac68c31b26febfce64d30c4
Sandeep <sandeepsreekumar75@gmail.com>
2019-04-28 15:56:15 +0530

    react state and props

--- src/posts/PostDetails.js
+++ src/posts/PostDetails.js
@@ -1,11 +1,31 @@
 import React, { Component } from 'react'
 
 class PostDetails extends Component {
+  constructor (props) {
+  	super(props)
+  	this.titleWasClicked = this.titleWasClicked.bind(this)
+  	this.toggleContent = this.toggleContent.bind(this)
+  	this.state = {
+  		toggleFlag: true
+  	}
+  }
+  titleWasClicked (event) {
+  	event.preventDefault()
+  	console.log(this.props)
+  }
+  toggleContent (event) {
+  	event.preventDefault()
+  	this.setState({
+  		toggleFlag: !this.state.toggleFlag
+  	})
+  }
   render () {
+  	const { toggleFlag } = this.state
     const { post } = this.props
     return (
       <div>
-        <b>{post.title} on {post.date}</b> :{post.content}
+        <b onClick={this.titleWasClicked}>{post.title} on {post.date}</b> {toggleFlag === true ? <p>{post.content}</p> : ''}
+        <button onClick={this.toggleContent}>Toggle content</button>
       </div>
     )
   }
